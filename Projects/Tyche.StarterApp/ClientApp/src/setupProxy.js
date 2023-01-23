1bda9853fcdefb69fd115af848e7d643e10fd004
Kalmar99 <jonas_kr@live.no>
2023-01-23 20:03:53 +0100

    [patch] setup request proxy & health endpoint

--- Projects/Tyche.StarterApp/ClientApp/src/setupProxy.js
+++ Projects/Tyche.StarterApp/ClientApp/src/setupProxy.js
@@ -4,12 +4,12 @@ const { env } = require('process');
 const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
   env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'http://localhost:8080';
 
-const context =  [
-  "/weatherforecast",
+const endpointsToProxy =  [
+  "/api/health",
 ];
 
 module.exports = function(app) {
-  const appProxy = createProxyMiddleware(context, {
+  const appProxy = createProxyMiddleware(endpointsToProxy, {
     target: target,
     secure: false,
     headers: {
---
+++ Projects/Tyche.StarterApp/Health/HealthController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SaasTemplate.Health;
+
+[ApiController]
+[Route("api/health")]
+public class HealthController : ControllerBase
+{
+    [HttpGet]
+    public async Task<bool> IsHealthy()
+    {
+        return await Task.FromResult(true);
+    }
+}
