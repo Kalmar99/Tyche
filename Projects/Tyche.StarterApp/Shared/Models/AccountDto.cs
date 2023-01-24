244a4d3321497507146f58e3b76e6cff3a94609c
Kalmar99 <jonas_kr@live.no>
2023-01-24 18:52:30 +0100

    [patch] add user & account dto's

---
+++ Projects/Tyche.StarterApp/Shared/Models/AccountDto.cs
@@ -0,0 +1,17 @@
+﻿namespace Tyche.StarterApp.Shared;
+
+public class AccountDto
+{
+    public AccountDto(IReadOnlyCollection<UserDto> users, string name, bool isCompanyAccount)
+    {
+        Users = users;
+        Name = name;
+        IsCompanyAccount = isCompanyAccount;
+    }
+    
+    private IReadOnlyCollection<UserDto> Users { get; }
+
+    public string Name { get; }
+
+    public bool IsCompanyAccount { get; }
+}
---
+++ Projects/Tyche.StarterApp/Shared/Models/UserDto.cs
@@ -0,0 +1,29 @@
+﻿namespace Tyche.StarterApp.Shared;
+
+public class UserDto
+{
+    public UserDto(string id, string email, string password, UserRole role, string accountId)
+    {
+        Id = id;
+        Email = email;
+        Password = password;
+        Role = role;
+        AccountId = accountId;
+    }
+
+    public string Id { get; }
+    
+    public string Email { get; }
+
+    public string Password { get; }
+
+    public UserRole Role { get; }
+
+    public string AccountId { get; }
+}
+
+public enum UserRole
+{
+    User = 0,
+    AccountAdmin = 0,
+}
