1ced2af5513817d4de13c54fbb13522460018329
Kalmar99 <jonas_kr@live.no>
2023-02-02 20:36:06 +0100

    [patch](#1) add identity controller

---
+++ Projects/Tyche.StarterApp.Web/Identity/IdentityController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Tyche.StarterApp.Identity;
+
+[Route("api/identity")]
+public class IdentityController : ControllerBase
+{
+    private readonly IIdentityOrchestrator _orchestrator;
+
+    public IdentityController(IIdentityOrchestrator orchestrator)
+    {
+        _orchestrator = orchestrator;
+    }
+
+    [HttpPost("login")]
+    public async Task<IActionResult> Login()
+    {
+        //TODO: get user and pass it to identity
+        return Ok();
+    }
+}
