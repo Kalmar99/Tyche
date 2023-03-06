e88fc4eea28996d0a652e37925e09438e7db6f63
Kalmar99 <jonas_kr@live.no>
2023-03-06 18:31:44 +0100

    [patch] rebase this

---
+++ Tests/Tyche.StarterApp.Tests/Identity/Services/PasswordHasherTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Xunit;
+
+namespace Tyche.StarterApp.Tests.Identity.Services;
+
+public class PasswordHasherTests
+{
+    [Fact]
+    public void GenerateHash_ReturnsHashForString()
+    {
+        throw new NotImplementedException();
+    }
+
+    [Fact]
+    public void VerifyHash_ReturnsTrue_WhenPasswordsMatch()
+    {
+        throw new NotImplementedException();
+    }
+}
